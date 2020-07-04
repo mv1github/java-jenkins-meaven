6a061c49debc6756612aa93e9828febcafcc6a8c
mv1github <moasis_99@yahoo.com>
2020-07-03 19:45:30 -0700

    added initial jenkins file

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,11 +1,13 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.*;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.After;
-import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
